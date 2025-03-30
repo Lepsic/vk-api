f2a725a1123879f49ccaa5842fde7c781723c76f
lepsick <Lepsick2003@yandex.ru>
2025-03-31 01:51:50 +0300

    ref

--- app/pkg/models/app/__init__.py
+++ app/pkg/models/app/__init__.py
@@ -2,5 +2,4 @@ from app.pkg.models.app.schema import user as user_schema
 from app.pkg.models.app.sqlalchemy import user as user_sql
 
 from app.pkg.models.app.schema import auth
-from app.pkg.models.app.schema import transaction
 from app.pkg.models.app.schema import vk_api
